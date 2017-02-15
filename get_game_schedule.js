40dd385be6f9daf9a0a543dfe77ab827c8f66914
Goto Ryuta <goto@crafdea.co.jp>
2017-02-15 15:35:55 +0900

    デバッグ用メソッドの削除し、各処理を関数にまとめた。

--- get_game_schedule.js
+++ get_game_schedule.js
@@ -5,32 +5,52 @@ const json2csv = require('json2csv');
 const dateUtils = require('date-utils');
 
 /**
- * write data to the specified file path
- * @param string filePath
- * @param mix data
- * @param bool isAppend (true:file append / false:file overwrite)
+ * NPBの公式HPから公式戦の情報を取得する
+ * @return array gameData 試合情報
  */
-const writeData = function(filePath, data, isAppend) {
-    if(isAppend) {
-        fs.appendFile(filePath, util.inspect(data) , function(err) {
-            if(err){
-                console.log(err);
-                return false;
-            }
-        });
-    }else{
-        fs.writeFile(filePath, util.inspect(data) , function(err) {
-            if(err){
-                console.log(err);
-                return false;
-            }
-        });
+const getGameData = () => {
+    var gameData = [];
+    const startMonth = 3;
+    const endMonth = 12;
+    const baseUrl = "http://npb.jp/games/2017/";
+    for(month=startMonth;month<endMonth;month++) {
+        var accessUrl = baseUrl + "schedule_" + ("0"+month).slice(-2) + "_detail.html";
+        var fetchResult = accessNPBWebPage(accessUrl);
+        if(fetchResult){
+            fetchResult.$('.table_normal.summary_table > table > tbody > tr').each(function (idx) {
+                //試合日の取得
+                var dateThTag = fetchResult.$(this).children('th');
+                if(dateThTag.get(0)){
+                    date = dateThTag.text();
+                }
+                gameData.push(getGameDetail(fetchResult.$(this), date));
+            });
+        }
     }
-    return true;
+    return gameData;
 }
 
-const getGameData = function (tableObj, date){
-    let gameData = {
+/**
+ * NPB公式HPへアクセス
+ */
+const accessNPBWebPage = (accessUrl) => {
+    console.log("access to " + accessUrl);
+    var fetchResult = client.fetchSync(accessUrl);
+    if(!fetchResult.error){
+        return fetchResult;
+    }else{
+        console.log("access error. => url: "+ accessUrl);
+        console.log(fetchResult.error);
+        return false;
+    }
+}
+
+/**
+ * 各試合の詳細情報を取得する
+ * @return array gameData
+ */
+const getGameDetail = (tableObj, date) => {
+    var gameData = {
         "date": date,
         "team1": tableObj.find('div.team1').text().replace(/\s+/g, ""),
         "team2": tableObj.find('div.team2').text().replace(/\s+/g, ""),
@@ -46,44 +66,26 @@ const getGameData = function (tableObj, date){
     return gameData;
 }
 
-// Access NPB schedule page
-var dt = new Date();
-var file_name = dt.toFormat("YYYY_MM_DD_HH24MISS") + '_gameData.csv';
-const filePath = './get-data/' + file_name;
-const fields = [
-    "date","team1","team2","score1","score2","place","time","weather","comment","pit1","pit2"
-];
-var gameData = [];
-var date = "";
-var baseUrl = "http://npb.jp/games/2017/";
-for(month=3;month<12;month++) {
-    var accessUrl = baseUrl + "schedule_" + ("0"+month).slice(-2) + "_detail.html";
-    console.log("access to " + accessUrl);
-    var fetchResult = client.fetchSync(accessUrl);
-    if(!fetchResult.error){
-        fetchResult.$('.table_normal.summary_table > table > tbody > tr').each(function (idx) {
-            var dateThTag = fetchResult.$(this).children('th');
-            if(dateThTag.get(0)){
-                date = dateThTag.text();
+const main = () => {
+    const dt = new Date();
+    const file_name = dt.toFormat("YYYY_MM_DD_HH24MISS") + '_gameData.csv';
+    const filePath = './get-data/' + file_name;
+    const fields = [
+        "date","team1","team2","score1","score2","place","time","weather","comment","pit1","pit2"
+    ];
+    const gameData = getGameData();
+    try {
+        var csvData = json2csv({ data: gameData, fields: fields });
+        fs.writeFile(filePath, csvData, function(err) {
+            if(err){
+                console.log(err);
+            }else{
+                console.log("wrote to " + filePath);
             }
-            gameData.push(getGameData(fetchResult.$(this), date));
         });
-    }else{
-        console.log("access error. => url: "+ accessUrl);
-        console.log(fetchResult.error);
+    } catch (err) {
+        console.error(err);
     }
 }
 
-try {
-    var csvData = json2csv({ data: gameData, fields: fields });
-
-    fs.writeFile(filePath, csvData, function(err) {
-        if(err){
-            console.log(err);
-        }else{
-            console.log("wrote to " + file_name);
-        }
-    });
-} catch (err) {
-    console.error(err);
-}
+main();
